00a5d084dae5725d49c7c6d16af5636902268c36
AbhishekTiwari07 <abhishekt2024@gmail.com>
2021-12-12 12:47:20 +0530

    Added user model

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const { Schema } = mongoose;
+const userSchema = new Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    bio: String,
+    public_address: {
+        type: String,
+        required: true,
+    }
+},{
+    timestamps: true
+});
+
+userSchema.pre('save', async function(next) {
+    if(this.isModified('password'))
+        this.password = await bcrypt.hash(this.password,8);
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
